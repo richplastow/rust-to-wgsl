4a11f9cca6993a4894d254772fb0733d8c210aa4
richplastow <rich@richplastow.com>
2024-12-19 15:43:32 +0000

    adds isWhitespaceMost() and isWhitespaceRare() utilities

--- test.mjs
+++ test.mjs
@@ -3,6 +3,7 @@ import { testExample02 } from './examples/run-02.mjs';
 import { testExample03 } from './examples/run-03.mjs';
 import { testExample04 } from './examples/run-04.mjs';
 
+import { testCategoriseCharacter } from './lib/roughly-parse-rust/categorise-character.test.mjs';
 import { testHighlightWGSL } from './lib/highlight-wgsl/highlight-wgsl.test.mjs';
 import { testRoughlyParseRust } from './lib/roughly-parse-rust/roughly-parse-rust.test.mjs';
 import { testTransformParts } from './lib/transform-parts/transform-parts.test.mjs';
@@ -16,6 +17,7 @@ testExample03();
 testExample04();
 
 // Library.
+testCategoriseCharacter();
 testHighlightWGSL();
 testRoughlyParseRust();
 testTransformParts();
